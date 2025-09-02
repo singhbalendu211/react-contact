babe631edf7e51e5c9899d59766b15d19ad2972d
singhbalendu211 <singhbalendu1@gmail.com>
2025-09-02 19:52:26 +0530

    modal updated

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -6,7 +6,7 @@ const Modal = ({onClose, isOpen, children}) => {
   return createPortal(
    <> {isOpen && (
             <div className='grid place-items-center absolute top-0 z-40 backdrop-blur h-screen w-screen'>
-                <div className='m-auto z-50 relative min-h-[200px] min-w-[50%] bg-white p-4 rounded-2xl'>
+                <div className='m-auto z-50 relative min-h-[200px] min-w-[80%] bg-white p-4 rounded-2xl'>
 
                  <div className='flex justify-end'>
                    <AiOutlineClose onClick={onClose} className='self-end text-2xl' />
